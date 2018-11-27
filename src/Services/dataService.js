95bbc79ef963ff44ee66a4a1b5f82ec552d55281
DimaSerbenyuk <d_serbenyuk@mail.ru>
2018-11-27 23:45:04 +0200

    add project

---
+++ src/Services/dataService.js
@@ -0,0 +1,25 @@
+import axios from 'axios'
+
+
+// noinspection JSAnnotator
+const apiKey: string = '30beb83765e9e6c574fdc62c7ae1a000';
+
+// noinspection JSAnnotator
+export default {
+    getMovies: (category: string) =>{
+        const url = `https://api.themoviedb.org/3/movie/${category}?api_key=${apiKey}&language=en-US&page=1`;
+        return axios.get(url).then(info => info.data)
+    },
+    getSearch: (query: string) => {
+        const url = `https://api.themoviedb.org/3/search/movie?query=${query}&api_key=${apiKey}`;
+        return axios.get(url).then(info => info.data)
+    },
+    getMovieById: (movieId: number) => {
+        const url = `https://api.themoviedb.org/3/movie/${movieId}?api_key=${apiKey}&append_to_response=videos`;
+        return axios.get(url).then(info => info.data)
+    },
+    getMostVoted: () => {
+        const url = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=en-US&sort_by=vote_average.asc&include_adult=true&include_video=false&page=1`;
+        return axios.get(url).then(info => info.data)
+    }
+}
